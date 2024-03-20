f5e0c85a28a8ad1fef37452da1cdc08310c571cf
aydinSerbest <aydinserbest34@gmail.com>
2024-03-20 09:48:09 +0100

    updated

--- src/test/java/bddTraders/WhenCreatingANewClient.java
+++ src/test/java/bddTraders/WhenCreatingANewClient.java
@@ -1,10 +1,12 @@
 package bddTraders;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 
@@ -16,12 +18,16 @@ public class WhenCreatingANewClient {
     }
 
     @Test
+    @DisplayName("sending body with string")
     public void each_new_client_should_have_a_unique_id() {
-        String newClient = "{\n" +
-                "  \"firstName\": \"john\",\n" +
-                "  \"lastName\": \"white\",\n" +
-                "  \"email\": \"blue\"\n" +
-                "}";
+
+        //this is a text block
+        String newClient = """
+                {
+                  "firstName": "john",
+                  "lastName": "white",
+                  "email": "blue@gmail.com"
+                }""";
 
         given().body(newClient)
                 //.contentType("application/json")  OR:
@@ -29,7 +35,8 @@ public class WhenCreatingANewClient {
                 .when().post("/client")
                 .then()
                 .statusCode(200)
-                .and().body("id", not(equalTo(0)));
+                .and().body("id", not(equalTo(0)))
+                .and().body("email", equalTo("blue@gmail.com"));
     }
     /*
     if we do not mention that we are sending json data with:
@@ -41,4 +48,32 @@ public class WhenCreatingANewClient {
     from the same library
     if one comes from a different library it gives error
      */
+    @Test
+    @DisplayName("sending body with domain object")
+    public void each_new_client_should_have_a_unique_id2() {
+
+       Client newClient = new Client("michael", "white", "blue@gmail.com");
+        given().body(newClient)
+                //.contentType("application/json")  OR:
+                .header("Content-Type", "application/json")
+                .when().post("/client")
+                .then()
+                .statusCode(200)
+                .and().body("id", not(equalTo(0)))
+                .and().body("email", equalTo("blue@gmail.com"));
+    }
+    @Test
+    @DisplayName("retrieving data with domain object")
+    public void each_new_client_should_have_a_unique_id3() {
+
+        Client newClient = new Client("michael", "white", "blue@gmail.com");
+        Client response = given().body(newClient)
+                //.contentType("application/json")  OR:
+                .header("Content-Type", "application/json")
+                .when().post("/client")
+                .as(Client.class);//this is a domain object;
+
+        System.out.println(response.firstName());
+        assertThat(response.firstName()).isEqualTo("michael");
+    }
 }
